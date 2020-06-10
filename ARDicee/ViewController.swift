649cad7c399ffd3fe107524f9ca5425d801e22c1
FillipiPS <fillipi_ps@outlook.com>
2020-06-10 19:03:10 -0300

    Add a Cube Programmatically

--- ARDicee/ViewController.swift
+++ ARDicee/ViewController.swift
@@ -20,11 +20,24 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Set the view's delegate
         sceneView.delegate = self
         
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        let cube = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.01)
+        let material = SCNMaterial()
         
-        // Set the scene to the view
-        sceneView.scene = scene
+        material.diffuse.contents = UIColor.red
+        cube.materials = [material]
+        
+        let node = SCNNode()
+        node.position = SCNVector3(0, 0.1, -0.5)
+        node.geometry = cube
+        
+        sceneView.scene.rootNode.addChildNode(node)
+        sceneView.autoenablesDefaultLighting = true
+        
+        // Create a new scene
+//        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+//
+//        // Set the scene to the view
+//        sceneView.scene = scene
     }
     
     override func viewWillAppear(_ animated: Bool) {
