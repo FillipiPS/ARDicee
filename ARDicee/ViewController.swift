170ba83b04b3c1c5d4ce325f442f791177d2df5e
FillipiPS <fillipi_ps@outlook.com>
2020-06-12 19:46:31 -0300

    Animate 3D Objects

--- ARDicee/ViewController.swift
+++ ARDicee/ViewController.swift
@@ -73,8 +73,19 @@ class ViewController: UIViewController, ARSCNViewDelegate {
                     )
                     
                     sceneView.scene.rootNode.addChildNode(diceNode)
+                    
+                    let randomX = Float(arc4random_uniform(4) + 1) * (Float.pi / 2)
+                    let randomZ = Float(arc4random_uniform(4) + 1) * (Float.pi / 2)
+                    
+                    diceNode.runAction(
+                        SCNAction.rotateBy(
+                            x: CGFloat(randomX * 5),
+                            y: 0,
+                            z: CGFloat(randomZ * 5),
+                            duration: 0.5
+                        )
+                    )
                 }
-                print(hitResult)
             }
         }
     }
